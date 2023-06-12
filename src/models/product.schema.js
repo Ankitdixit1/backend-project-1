27cc8b50678a18deac62778cedeff4e9b7bf8936
Ankit dixit <ankitdixit29269@gmail.com>
2023-06-12 20:21:35 +0000

    some more models

---
+++ src/models/product.schema.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const   productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:["true","plz provide a product name" ],
+        trim:true,
+        maxleng:[120,"product name should look "]
+    },
+    price:{
+        type:String,
+        required:["true","plz provide a product price" ],
+        maxleng:[5,"product price shoulde be not more   5 "]
+    },
+    Description:{
+        type:String,
+
+    },
+    photo:[
+        {
+            Secure_url:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    Stock:{
+        type:Number,
+        default:0
+    },
+    sold:{
+        type:Number,
+        default:0
+    },
+    CollectionID:{
+
+    }
+},{timestanps: true})
+
+export default mongoose.model("product",productSchema)
