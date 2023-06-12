55524f7c4669169ae6a43da5e79e8ce5a40b08b6
Ankit dixit <ankitdixit29269@gmail.com>
2023-06-12 11:41:23 +0000

    hooks and methords in moongose

--- src/models/users.schema.js
+++ src/models/users.schema.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import AuthRoles from "../utills/authrole.js";
+import bcrypt from "bcryptjs"; 
 const userschema = new mongoose.Schema(
     {
         name:{
@@ -26,4 +27,18 @@ const userschema = new mongoose.Schema(
         forgotpasswrodToken:Date
     },{timestamps:true}
 )
+// encripting the password before saving
+userschema.pre("save", async function(next){
+    if (!this.isModified("passwrod")) return next()
+    this.password = await bcrypt.hash(this.password,10) 
+    next()
+})
+userschema.methods={
+    //compare passwords 
+    comparePassword:async function(comparePassword){
+        return await bcrypt.compare(comparePassword,this.passward)
+    }
+}
+
+
 export default mongoose.model("User",userschema)
